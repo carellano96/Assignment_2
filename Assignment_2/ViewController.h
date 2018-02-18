657e92c6bdcb8ea83bcb50623fae27c03884771a
carellano96 <carlos@172-17-80-28.dynapool.nyu.edu>
2018-02-17 19:57:12 -0500

    Initial Commit

---
+++ Assignment_2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Assignment_2
+//
+//  Created by carlos arellano on 2/17/18.
+//  Copyright © 2018 nyu.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Assignment_2/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Assignment_2
+//
+//  Created by carlos arellano on 2/17/18.
+//  Copyright © 2018 nyu.edu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
